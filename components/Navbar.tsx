823550b7b9ef56e5125511c865b2efd5ca941da5
wizeeyung <ogunbanwosamson@gmail.com>
2024-10-26 03:43:39 +0100

    padding fix

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,7 +6,7 @@ import ListMenu from './ListMenu'
 
 const Navbar = () => {
   return (
-    <nav className='w-full fixed top-0 z-50 text-white py-7 border-b-2 bg-black-100 border-black-200'>
+    <nav className='w-full fixed top-0 z-50 text-white py-7 px-4 border-b-2 bg-black-100 border-black-200'>
       <div className='flex justify-between mx-auto w-full max-w-screen-2xl xs:px-6 sm:px-16'>
         <Link href='/'>
           <Image 
